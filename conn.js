3cbc9a16d679133b8e713bbc6668c1ab4ae5bb9a
SujalDhiman <sujaldhiman2003@gmail.com>
2023-10-10 23:31:35 +0530

    front

--- conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose
-  .connect("mongodb://localhost:27017/form")
-  .then(() => {
-    console.log("connection successful");
-  })
-  .catch((e) => {
-    console.log(e);
-  });
